90f3e0aedd7f4d8da787bb913d2c789f51ad0187
Viktor Indyk <Viktor.Indyk@playtech.com>
2017-05-12 15:42:30 +0300

    changes

--- test/homepage.js
+++ test/homepage.js
@@ -8,8 +8,8 @@ describe('Home page', function() {
     it('should have the right title', function() {
         browser.windowHandleMaximize(['current']);
         browser.url('/');
-        var title = browser.getTitle();
-        assert.equal(title, 'SCasino - Online Casino that Allows You to Play Your Way - SCasino');
+            var title = browser.getTitle();
+            assert.equal(title, 'NAME - Online Casino that Allows You to Play Your Way - NAME');
         });
 
                                     /*LOGIN PART*/
@@ -17,15 +17,15 @@ describe('Home page', function() {
     it('should be possible to open login form', function () {
         browser.waitForVisible('.btn_action_login', 5000);
         browser.click('.btn_action_login');
-        var rememberMe = browser.getText(".field_name_remember-me");
-        assert.equal(rememberMe, 'Remember me');
+            var rememberMe = browser.getText(".field_name_remember-me");
+            assert.equal(rememberMe, 'Remember me');
         });
 
     it('should be possible to close login form', function () {
         browser.click('.fn-close');
         browser.pause(2000)
-        var loginPopup = browser.isVisible('.popup_type_login');
-        assert.equal(loginPopup, false);
+            var loginPopup = browser.isVisible('.popup_type_login');
+            assert.equal(loginPopup, false);
         });
 
     it('should be possible to login', function () {
@@ -34,16 +34,16 @@ describe('Home page', function() {
         browser.setValue('[name=password]','Password1');
         browser.click('.btn_type_popup-login');
         browser.pause(1000)
-        var username = browser.getText('.main-header__user-name--name');
-        assert.equal(username, 'USER2618');
+            var username = browser.getText('.main-header__user-name--name');
+            assert.equal(username, 'USER2618');
         });
 
     it('should be possible to logout', function () {
         browser.click('.btn_type_logout');
         browser.click('.fn-accept');
         browser.pause(1000)
-        var loginButton = browser.isVisible('.btn_action_login');
-        assert.equal(loginButton, true);
+            var loginButton = browser.isVisible('.btn_action_login');
+            assert.equal(loginButton, true);
         });
 
                                     /*REGISTRATION PART*/
@@ -51,8 +51,8 @@ describe('Home page', function() {
     it('should be possible to open registration form', function () {
         browser.click('.btn_action_sign-up');
         browser.pause(1000)
-        var registerTitle = browser.getText(".registration__title-container");
-        assert.equal(registerTitle, 'Registration');
+            var registerTitle = browser.getText(".registration__title-container");
+            assert.equal(registerTitle, 'Registration');
         });
 
     it('should be possible to register', function () {
@@ -66,7 +66,7 @@ describe('Home page', function() {
         browser.selectByIndex('#day', 1);
         browser.selectByIndex('#month', 1);
         browser.selectByIndex('#year', 10);
-        browser.setValue('[name=email]','test@playtech.com');
+        browser.setValue('[name=email]','test@NAME.com');
         browser.click('.fn-next');
 
                 //Second step
@@ -87,12 +87,11 @@ describe('Home page', function() {
         browser.click('#terms-checkbox');
         browser.click('.btn_type_register');
         browser.pause(2000)
-        //Verify that "My Promotions" page is opened after registration.
-        var promotionTitle = browser.getTitle();
-        assert.equal(promotionTitle, 'My Promotions - SCasino');
-        //Verify  that user is logged in after registration
-        var logoutbtn = browser.isVisible('.btn_type_logout');
+                //Verify that "My Promotions" page is opened after registration.
+            var promotionTitle = browser.getTitle();
+            assert.equal(promotionTitle, 'My Promotions - NAME');
+                //Verify  that user is logged in after registration
+            var logoutbtn = browser.isVisible('.btn_type_logout');
             assert.equal(logoutbtn, true);
-        browser.pause(10000)
         });
 });
